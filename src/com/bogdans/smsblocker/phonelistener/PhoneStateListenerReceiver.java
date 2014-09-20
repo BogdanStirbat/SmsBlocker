458fbafddb7f651900bfc6d3b5672be26e48265a
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2014-09-20 16:26:03 +0300

    added permisiion and registered BroadcastReceiver; TODO: remove the registered Receiver from AndroidManifest and start/stop the BroadcastReceiver from an Service

--- src/com/bogdans/smsblocker/phonelistener/PhoneStateListenerReceiver.java
+++ src/com/bogdans/smsblocker/phonelistener/PhoneStateListenerReceiver.java
@@ -2,9 +2,13 @@ package com.bogdans.smsblocker.phonelistener;
 
 import java.lang.reflect.Method;
 
+import com.bogdans.smsblocker.constants.SharedPreferencesConstants;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 
 /**
@@ -19,6 +23,10 @@ public class PhoneStateListenerReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals("android.intent.action.PHONE_STATE")) {
+			checkSettings(context);
+			if (!blockCalls) {
+				return;
+			}
 			String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 			if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
 				String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
@@ -28,7 +36,6 @@ public class PhoneStateListenerReceiver extends BroadcastReceiver {
 			}
 		}
 	}
-	
 	
 	public boolean killCall(Context context) {
 		try {
@@ -48,6 +55,14 @@ public class PhoneStateListenerReceiver extends BroadcastReceiver {
 			return false;
 		}
 		return true;
+	}
+	
+	private void checkSettings(Context context) {
+		SharedPreferences preferences = context
+				.getSharedPreferences(
+						SharedPreferencesConstants.PREF_FILE_NAME,
+						Context.MODE_PRIVATE);
+		blockCalls = preferences.getBoolean(SharedPreferencesConstants.ACTIVE, false);
 	}
 
 }
