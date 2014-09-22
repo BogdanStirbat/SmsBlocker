249130d5bdc245987e76cda15bbe20e69398668c
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2014-09-22 23:06:47 +0300

    added BroadcastReceiver; tested how to call device:  telnet localhost 5554;  gsm call 123456789

---
+++ src/com/bogdans/smsblocker/constants/IntentContstants.java
@@ -0,0 +1,7 @@
+package com.bogdans.smsblocker.constants;
+
+public class IntentContstants {
+	
+	/** Used to pass phone number from Broadcast receiver to Service */
+	public static final String PHONE_NUMBER = "PHONE_NUMBER";
+}
